e6b4f80c4bb3ebd4cc4bcfcd6d8295f35f13faf7
xbepxx@gmail.com <xbepxx@gmail.com>
2021-10-29 11:42:20 +0300

    Добавьте файлы проекта.

---
+++ HW14/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal (string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.Write(Name);
+            Say();
+        }
+    }
+    class Cat: Animal
+    {
+        string name;
+        public Cat (string name)
+            :base (name)
+        {
+
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void ShowInfo()
+        {
+            Console.Write(Name);
+            Say();
+        }
+    }
+    class Dog : Animal
+    {
+        string name;
+        public Dog (string name)
+            : base(name)
+        {
+
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void ShowInfo()
+        {
+            Console.Write(Name);
+            Say();
+        }
+    }
+}
